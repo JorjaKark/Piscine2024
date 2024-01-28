981210699a770539d6a8386904722b76b831bea1
Mafalda Dias <138318938+JorjaKark@users.noreply.github.com>
2024-01-28 00:19:32 +0000

    Add files via upload

---
+++ C02/ex10/ft_strlcpy.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mafferre <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/17 16:29:48 by mafferre          #+#    #+#             */
+/*   Updated: 2024/01/20 13:35:50 by mafferre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	i = 0;
+	while (src[i])
+	{
+		i ++;
+	}
+	return (i);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	char src[] = "Helloooo";
+	char dest[] = "Worldddd";
+	printf("%d | %s", ft_strlcpy(dest, src, 10), dest);
+}
+*/
